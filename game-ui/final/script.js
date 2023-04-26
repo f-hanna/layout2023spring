60a020cfc3875435c58d5f53d16e286ee53bed3a
f-hanna <77943310+f-hanna@users.noreply.github.com>
2023-04-26 16:01:44 -0500

    :')

---
+++ game-ui/final/script.js
@@ -0,0 +1,80 @@
+
+const atkMoves = ["Use Dagger" + '<img class="small-icon" src="images/dagger-icon.png" alt="dagger"></img>', "Use Sword" + '<img class="small-icon" src="images/sword-icon.png" alt="dagger"></img>'];
+const skillMoves = ["Lion Dance SP: 4", "Moonlight Waltz SP: 7", "Peacock Strut SP: 4", "Mole Dance SP: 4", "Night Ode SP: 10", "Panther Dance SP: 4", "Bewildering Grace SP: 25", "Sealticge's Seduction SP: 30"];
+
+
+
+function skillDetail(e) {
+
+    var parentDiv = document.getElementById("player-info-detail")
+    var parentWrapper = parentDiv.parentNode;
+    e = e || window.event;
+
+
+
+    if (parentDiv.style.display === "none") {
+        parentDiv.style.display = "block";
+        // parentWrapper.insertBefore(parentDiv, parentWrapper.firstChild);
+        //check which option was clicked
+        let moveArr = [];
+        if (e.id == "atk-option") {
+            moveArr = atkMoves;
+
+
+        }
+        else if (e.id == "skill-option") {
+            moveArr = skillMoves;
+        }
+        else {
+            moveArr = [];
+        }
+
+        parentDiv.innerHTML = "";
+        //parentDiv.style.display = block;
+
+        for (i = 0; i < moveArr.length; i++) {
+            $('#player-info-detail').append('<div class="player-options">' + moveArr[i] + '</div>');
+            //console.log(option)
+        }
+
+    } else {
+        parentDiv.style.display = "none";
+    }
+
+
+    //alert("clicked " + e.id);
+
+
+
+}
+
+function primroseBar(e) {
+    // alert("touched prim")
+
+    document
+        .getElementById('primrose-hp')
+        .classList
+        .toggle('low')
+}
+
+function opheliaPress(e) {
+
+    document
+        .getElementById('ophelia-bar')
+        .classList
+        .toggle('character-bg')
+
+    document
+        .getElementById('primrose-bar')
+        .classList
+        .toggle('character-bg')
+
+
+
+    document.getElementById("player-img").src = "images/ophelia.png"
+
+    document.getElementById("background").src = "images/bg-with-people-2.png"
+
+
+
+}
