3803de5b7ebb8d9190a5e4342ec4931a4294340f
f-hanna <77943310+f-hanna@users.noreply.github.com>
2023-04-26 17:34:42 -0500

    hover!

--- game-ui/final/script.js
+++ game-ui/final/script.js
@@ -1,6 +1,17 @@
 
 const atkMoves = ["Use Dagger" + '<img class="small-icon" src="images/dagger-icon.png" alt="dagger"></img>', "Use Sword" + '<img class="small-icon" src="images/sword-icon.png" alt="dagger"></img>'];
-const skillMoves = ["Lion Dance SP: 4", "Moonlight Waltz SP: 7", "Peacock Strut SP: 4", "Mole Dance SP: 4", "Night Ode SP: 10", "Panther Dance SP: 4", "Bewildering Grace SP: 25", "Sealticge's Seduction SP: 30"];
+const skillMoves = ["Lion Dance SP: 4", "Moonlight Waltz SP: 7" + '<img class="small-icon" src="images/dark-icon.png" alt=""></img>', "Peacock Strut SP: 4", "Mole Dance SP: 4", "Night Ode SP: 10" + '<img class="small-icon" src="images/dark-icon.png" alt=""></img>', "Panther Dance SP: 4", "Bewildering Grace SP: 25", "Sealticge's Seduction SP: 30"];
+const skillDescription = [
+    "Augment a single ally's physical attacks for 2 turns.",
+    "Deal heavy dark damage to a single foe.",
+    "Augment a single ally's elemental attacks for 2 turns.",
+    "Augment a single ally's physical defense for 2 turns.",
+    "Deal dark damage to all foes.",
+    "Augment a single ally's speed for 2 turns.",
+    "Cause a curious effect to occur one time.",
+    "[Divine Skill] For 3 turns, skills performed by a single ally that usually target one foe* will affect all foes instead (this will not affect divine skills)."
+
+]
 
 
 
@@ -10,8 +21,6 @@ function skillDetail(e) {
     var parentWrapper = parentDiv.parentNode;
     e = e || window.event;
 
-
-
     if (parentDiv.style.display === "none") {
         parentDiv.style.display = "block";
         // parentWrapper.insertBefore(parentDiv, parentWrapper.firstChild);
@@ -19,23 +28,29 @@ function skillDetail(e) {
         let moveArr = [];
         if (e.id == "atk-option") {
             moveArr = atkMoves;
+            parentDiv.innerHTML = "";
+            //parentDiv.style.display = block;
 
-
+            for (i = 0; i < moveArr.length; i++) {
+                $('#player-info-detail').append('<div class="player-options" data-hover="Does not cost SP">' + moveArr[i] + '</div>');
+                //console.log(option)
+            }
         }
         else if (e.id == "skill-option") {
             moveArr = skillMoves;
+            parentDiv.innerHTML = "";
+            //parentDiv.style.display = block;
+
+            for (i = 0; i < moveArr.length; i++) {
+                $('#player-info-detail').append('<div class="player-options" data-hover="' + skillDescription[i] + '">' + moveArr[i] + '</div>');
+                //console.log(option)
+            }
         }
         else {
             moveArr = [];
         }
 
-        parentDiv.innerHTML = "";
-        //parentDiv.style.display = block;
 
-        for (i = 0; i < moveArr.length; i++) {
-            $('#player-info-detail').append('<div class="player-options">' + moveArr[i] + '</div>');
-            //console.log(option)
-        }
 
     } else {
         parentDiv.style.display = "none";
@@ -76,5 +91,30 @@ function opheliaPress(e) {
     document.getElementById("background").src = "images/bg-with-people-2.png"
 
 
+
+}
+
+var shield = document.getElementById("shield-img");
+setInterval(updatePoints, 4000);
+
+let points = 5;
+
+function updatePoints() {
+
+    points--;
+
+    if (points == 0) {
+        $('#shield-img h3').css('visibility', 'hidden');
+        $('#shield-img').css('visibility', 'hidden');
+    }
+
+
+    $("#shield-img h3").text(points);
+}
+
+function nextTurn() {
+    var turnGroup = document.getElementById("turn-wrapper")
+    var turns = turnGroup.getElementsByClassName("turn-icon")
+    turnGroup.removeChild(turns[0])
 
 }
